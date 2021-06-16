c674d3c8098adb8dff4bd6c2817209aa223cadf4
Yehor Silahin <egor.silagin96@gmail.com>
2021-06-16 09:09:25 +0300

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //initialization
+        int b = 10;
         Scanner in = new Scanner(System.in);
         System.out.print("Input an integer number: ");
         int n = in.nextInt();
