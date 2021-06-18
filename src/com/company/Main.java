30751c94700ee0c8de96331b584537c5c46cb7be
YSilahin <egor.silagin96@gmail.com>
2021-06-18 16:35:14 +0300

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,32 +8,32 @@ public class Main {
 
         Scanner in = new Scanner(System.in);
         System.out.print("Input an integer number: ");
-        int n = in.nextInt();
+        int number = in.nextInt();
 
-        if (n <= 0) {
+        if (number <= 0) {
             System.out.print("Input a correct number.");
         }
-        int x = 0;
-        while (n != 1) {
+        int check = 0;
+        while (number != 1) {
 
-            if (n % 5 == 0) {
-                n /= 5;
+            if (number % 5 == 0) {
+                number /= 5;
 
-            } else if (n % 3 == 0) {
-                n /= 3;
+            } else if (number % 3 == 0) {
+                number /= 3;
 
-            } else if (n % 2 == 0) {
-                n /= 2;
-                
+            } else if (number % 2 == 0) {
+                number /= 2;
+
             } else {
                 System.out.print("It is not an ugly number.");
-                x = 1;
+                check = 1;
                 break;
             }
         }
-        if (x==0)
+        if (check==0)
             System.out.print("It is an ugly number.");
-        System.out.print("\n");
+            System.out.print("\n");
     }
 }
 
