603bb0f1f6c72264737bb6120c1820457127fc1b
YSilahin <egor.silagin96@gmail.com>
2021-06-23 12:12:35 +0300

    Little fix of output

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,37 +4,37 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static final String notUgly = "It's not an ugly number";
+    public static final String ugly = "It's an ugly number";
 
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Input an integer number: ");
         int number = in.nextInt();
 
         if (number <= 0) {
             System.out.print("Input a correct number.");
-        }
-        int check = 0;
-        while (number != 1) {
-
-            if (number % 5 == 0) {
-                number /= 5;
-
-            } else if (number % 3 == 0) {
-                number /= 3;
-
-            } else if (number % 2 == 0) {
-                number /= 2;
-
-            } else {
-                System.out.print("It is not an ugly number.");
-                check = 1;
-                break;
+        } else {
+            if (checkUglyNumber(number)) {
+                System.out.println(notUgly);
+            } else  {
+                System.out.println(ugly);
             }
         }
-        if (check==0)
-            System.out.print("It is an ugly number.");
-            System.out.print("\n");
+    }
+
+    public static boolean checkUglyNumber(int number) {
+        while (number != 1) {
+            if (number % 5 == 0) {
+                number /= 5;
+            } else if (number % 3 == 0) {
+                number /= 3;
+            } else if (number % 2 == 0) {
+                number /= 2;
+            } else {
+                return true;
+            }
+        }
+        return false;
     }
 }
-
-
